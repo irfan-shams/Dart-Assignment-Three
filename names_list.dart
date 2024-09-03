38df706234321d06cb69cf6a2d2f26e065aef78c
irfan-shams <shams.irfan@gmail.com>
2024-09-03 12:38:19 +0500

    Add Question-1 file

---
+++ names_list.dart
@@ -0,0 +1,6 @@
+// Q.1) Create a list of names and print all names using list.
+
+void main () {
+  List namesList = ['Basit', 'Ali', 'Kamran', 'Raza', 'Zafar', 'Saeed', 'Hanzala', 'Shoaib', 'Kashif', 'Qadeer'];
+  print(namesList);
+}
