f42791e1a1927384f8a1c4ba031be8955ed9629c
irfan-shams <shams.irfan@gmail.com>
2024-09-03 12:54:56 +0500

    Add Question-2 file

---
+++ days_list.dart
@@ -0,0 +1,6 @@
+// Q.2) Create a list of Days and print only Sunday.
+void main() {
+  List daysList = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  print(daysList[6]);
+  print(daysList.last);
+}
