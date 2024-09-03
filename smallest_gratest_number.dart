8b19dd06f91666387f5379af979320d2fdf4274b
irfan-shams <shams.irfan@gmail.com>
2024-09-03 18:30:38 +0500

    Updated with sort function

--- smallest_gratest_number.dart
+++ smallest_gratest_number.dart
@@ -1,5 +1,11 @@
 // Q.4) Create a list of numbers & write a program to get the smallest & greatest number from a list.
 
-void main() {  
-  print("Hello");
+void main() {
+  List<int> numberList = [5, 2, 7, 1000, 6, 90, 4, 8, 1, 3];
+  print(numberList);
+  numberList.sort();
+  var smallestValue = numberList[0];
+  var greatestValue = numberList[numberList.length -1];
+  print("Smallest value of the above list: $smallestValue");
+  print("Greatest value of the above list: $greatestValue");
 }
