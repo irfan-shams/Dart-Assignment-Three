fa2060b1d70710edead7d73e41bb688981add0d5
irfan-shams <shams.irfan@gmail.com>
2024-09-03 13:27:50 +0500

    Add Question-3 file

---
+++ student_list.dart
@@ -0,0 +1,8 @@
+// Q.3) Create a list  of name, class, roll no, grade, percentage. And print.
+
+void main() {
+  Map studentList = {'Name': 'Hamza', 'Class': 'B-Tech', 'Roll No': 147, 'Grade': 'B-Grade', 'Percentage': '86.5 %'};
+  print(studentList);
+  print(studentList.keys.toSet());
+  print(studentList.values.toSet());
+}
