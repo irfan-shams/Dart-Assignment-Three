581c37a57f1e665e6354cc9190ca215c294afb96
irfan-shams <shams.irfan@gmail.com>
2024-09-03 22:10:28 +0500

    Updated

--- negative_numbers.dart
+++ negative_numbers.dart
@@ -1,6 +1,10 @@
 // Q.7) Implement a Dart code that uses the where() method to filter out negative numbers from a list of integers. 
 // The program should take in the original list as a parameter and print a new list containing only the positive numbers.
 
-void main() {  
-  print("Hello");
+void main() {
+  List<int> numberList = [4, -3, 7, -1, 9, -6, 8, 2, -5, 10];
+  List<int> posNumbers = numberList.where((number) => number >= 0).toList();
+
+  print("Original list: $numberList");
+  print("Filtered list: $posNumbers");
 }
