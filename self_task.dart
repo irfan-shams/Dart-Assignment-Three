49114b4885dae45c32b3c763f21ded23c35be7f1
irfan.shams <shams.irfan@gmail.com>
2024-09-06 16:07:35 +0500

    Updated

--- self_task.dart
+++ self_task.dart
@@ -1,17 +1,22 @@
-// SELF TASK:
-// Use any 10 List methods.
-void main () {
+// SELF TASK: Use any 10 List methods.
+void main() {
   List<int> numberList = [2, 5, 1, 4, 7, 3, 10, 9, 6, 8];
   print("Original list: $numberList");
   numberList.sort();
   print("Sorted list: $numberList");
-  var rList = numberList.reversed.toList();
-  print("Reversed list: $rList");
+  var revList = numberList.reversed.toList();
+  print("Reversed list: $revList");
   print(numberList.length);
   print(numberList.first);
   print(numberList.last);
-  print(numberList[2]);  
+  print(numberList[2]);
   print(numberList.isEmpty);
   print(numberList.isNotEmpty);
   print(numberList.elementAt(7));
+  numberList.add(20);
+  print(numberList);
+  numberList.removeAt(0);
+  print(numberList);
+  numberList.insert(0, 1);
+  print(numberList);
 }
